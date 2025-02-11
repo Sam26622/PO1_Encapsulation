86d1dfae52663444f81cfba67300191e7ac49eb2
Sam26622 <2239299@cegepba.qc.ca>
2025-02-11 11:40:59 -0500

    Premier commit

---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        int pointDeVie;
+        int puissanceDAttaque;
+
+        public int PointDeVie { get => pointDeVie; set => pointDeVie = value; }
+        public int PuissanceDAttaque { get => puissanceDAttaque; set => puissanceDAttaque = value; }
+    }
+     
+}
